1be43926054c28a67fa5c7f45eaff0bcc33c1c97
Santhosh Sivan <santhoshkan205@gmail.com>
2026-03-14 12:31:51 +0530

    Block 1: Project init + Docker setup

---
+++ backend/src/main/java/com/runloyal/RunLoyalApplication.java
@@ -0,0 +1,16 @@
+package com.runloyal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableCaching
+public class RunLoyalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunLoyalApplication.class, args);
+    }
+}
